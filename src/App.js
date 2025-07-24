09b8a9524daa2b3a60c3c5909dcb00aa609b6065
Mehwishmushtaq <91072315+Mehwishmushtaq@users.noreply.github.com>
2025-07-24 18:49:31 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,16 +3,33 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Machines from './pages/Machines'
+import DevicePositioning from './pages/DevicePositioning'
+import Workflows from './pages/Workflows'
+import CalculationProcess from './pages/CalculationProcess'
+import VSPTrackerAction from './pages/VSPTrackerAction'
+import WhereWeFitIn from './pages/WhereWeFitIn'
+import ResourcesSupport from './pages/ResourcesSupport'
+import FAQSection from './components/home/FAQSection'
+
 
 const App = () => {
   return (
     <div>
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Home/>}/>
+          <Route path='/' element={<Home />} />
+          <Route path='/faq' element={<FAQSection />} />
+          <Route path='/machines' element={<Machines />} />
+          <Route path='/devices-positioning' element={<DevicePositioning />} />
+          <Route path='/workflows' element={<Workflows />} />
+          <Route path='/tools-calculators' element={<CalculationProcess />} />
+          <Route path='/vsp-tracker-in-action' element={<VSPTrackerAction />} />
+          <Route path='/where-we-fit-in' element={<WhereWeFitIn />} />
+          <Route path='/resources-support' element={<ResourcesSupport />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </Router>
     </div>
   )
